779743d3676a79f3389b97d0f292a0f6b63c6931
SpalenyBramborak <102430767+SpalenyBramborak@users.noreply.github.com>
2022-06-09 21:49:16 +0200

    Update __resource.lua

--- blackpdm_bymario/__resource.lua
+++ blackpdm_bymario/__resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-author 'Re-texture by: Mario/SpalenyBramborak, Dc: Le_Mario#9162'
+author 'Re-texture by: SpalenyBramborak Dc: Le_Mario#9162'
 
 
 this_is_a_map 'yes'
